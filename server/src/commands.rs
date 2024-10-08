883ea055a88abad408219753f6834c6240180fd1
Herman Brunberg <herman.brunberg@mio.se>
2024-10-08 21:14:11 +0200

    feat: implement basic websocket server

---
+++ server/src/commands.rs
@@ -0,0 +1,115 @@
+use crate::{Note, WebSocketResponse};
+use futures_util::{stream::SplitSink, SinkExt};
+use redis::{aio::MultiplexedConnection, AsyncCommands};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use warp::filters::ws::{Message, WebSocket};
+
+pub(crate) async fn handle_get_command(
+    ws_tx: &mut SplitSink<WebSocket, Message>,
+    redis_conn: Arc<Mutex<MultiplexedConnection>>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut conn = redis_conn.lock().await;
+
+    let keys: Vec<String> = match conn.keys::<&str, Vec<String>>("*").await {
+        Ok(keys) => keys,
+        Err(e) => {
+            send_response(ws_tx, 500).await;
+            return Err(Box::new(e));
+        }
+    };
+
+    let mut notes = Vec::new();
+    for key in keys {
+        let note: String = match conn.get::<&str, String>(&key).await {
+            Ok(note) => note,
+            Err(e) => {
+                send_response(ws_tx, 500).await;
+                return Err(Box::new(e));
+            }
+        };
+        notes.push(note);
+    }
+
+    let serialized_notes = match serde_json::to_string(&notes) {
+        Ok(sn) => sn,
+        Err(e) => {
+            send_response(ws_tx, 500).await;
+            return Err(Box::new(e));
+        }
+    };
+
+    match ws_tx.send(warp::ws::Message::text(serialized_notes)).await {
+        Ok(it) => it,
+        Err(e) => return Err(Box::new(e)),
+    };
+
+    Ok(())
+}
+
+pub(crate) async fn handle_set_command(
+    ws_tx: &mut SplitSink<WebSocket, Message>,
+    redis_conn: Arc<Mutex<MultiplexedConnection>>,
+    note: Note,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut conn = redis_conn.lock().await;
+
+    let serialized_note = match serde_json::to_string(&note) {
+        Ok(sn) => sn,
+        Err(e) => {
+            send_response(ws_tx, 500).await;
+            return Err(Box::new(e));
+        }
+    };
+
+    if let Err(e) = conn
+        .set::<&str, String, ()>(&note.id, serialized_note)
+        .await
+    {
+        send_response(ws_tx, 500).await;
+        return Err(Box::new(e));
+    }
+
+    send_response(ws_tx, 200).await;
+    info!("Saved note: {:?}", note.id);
+
+    Ok(())
+}
+
+pub(crate) async fn handle_delete_command(
+    ws_tx: &mut SplitSink<WebSocket, Message>,
+    redis_conn: Arc<Mutex<MultiplexedConnection>>,
+    id: String,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut conn = redis_conn.lock().await;
+
+    match conn.del::<&str, i16>(&id).await {
+        Ok(count) => {
+            if count == 0 {
+                error!("Failed to delete note since it does not exist: {:?}", id);
+                send_response(ws_tx, 404).await;
+                return Ok(());
+            }
+        }
+        Err(e) => {
+            send_response(ws_tx, 500).await;
+            return Err(Box::new(e));
+        }
+    };
+
+    send_response(ws_tx, 200).await;
+    info!("Deleted note: {:?}", id);
+
+    Ok(())
+}
+
+async fn send_response(ws_tx: &mut SplitSink<WebSocket, Message>, response: u16) {
+    let error_response = serde_json::to_string(&WebSocketResponse { response }).unwrap();
+
+    match ws_tx.send(warp::ws::Message::text(error_response)).await {
+        Ok(_) => {}
+        Err(e) => {
+            error!("Failed to send error response: {}", e);
+        }
+    }
+}
---
+++ server/src/main.rs
@@ -0,0 +1,122 @@
+mod commands;
+use commands::{handle_delete_command, handle_get_command, handle_set_command};
+use futures_util::{SinkExt, StreamExt};
+use redis::aio::MultiplexedConnection;
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use warp::{filters::ws::WebSocket, Filter};
+
+#[macro_use]
+extern crate log;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Note {
+    id: String,
+    content: String,
+    updated: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Command {
+    Get,
+    Set,
+    Delete,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct WebSocketMessage {
+    command: Command,
+    note: Option<Note>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct WebSocketResponse {
+    response: u16,
+}
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    let redis_client = redis::Client::open("redis://127.0.0.1/").unwrap();
+    let redis_conn = Arc::new(Mutex::new(
+        redis_client
+            .get_multiplexed_async_connection()
+            .await
+            .unwrap(),
+    ));
+
+    let routes = warp::path("echo")
+        .and(warp::ws())
+        .map(move |ws: warp::ws::Ws| {
+            let redis_conn = redis_conn.clone();
+            ws.on_upgrade(move |socket| connected(socket, redis_conn))
+        });
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+async fn connected(ws: WebSocket, redis_conn: Arc<Mutex<MultiplexedConnection>>) {
+    info!("Client connected");
+    let (mut ws_tx, mut ws_rx) = ws.split();
+
+    while let Some(msg) = ws_rx.next().await {
+        let msg = msg.unwrap();
+        if msg.is_text() {
+            let msg = msg.to_str().unwrap().to_string();
+            match serde_json::from_str::<WebSocketMessage>(&msg) {
+                Ok(message) => match message.command {
+                    Command::Get => {
+                        if let Err(e) = handle_get_command(&mut ws_tx, redis_conn.clone()).await {
+                            error!("Error while handling GET command: {}", e);
+                        }
+                    }
+                    Command::Set => {
+                        if let Some(note) = message.note {
+                            if let Err(e) =
+                                handle_set_command(&mut ws_tx, redis_conn.clone(), note).await
+                            {
+                                error!("Error while handling SET command: {}", e);
+                            }
+                        } else {
+                            error!("SET command received without a note");
+                            let error_response =
+                                serde_json::to_string(&WebSocketResponse { response: 400 })
+                                    .unwrap();
+                            ws_tx
+                                .send(warp::ws::Message::text(error_response))
+                                .await
+                                .unwrap();
+                        }
+                    }
+                    Command::Delete => {
+                        if let Some(note) = message.note {
+                            if let Err(e) =
+                                handle_delete_command(&mut ws_tx, redis_conn.clone(), note.id).await
+                            {
+                                error!("Error while handling DELETE command: {}", e);
+                            }
+                        } else {
+                            error!("DELETE command received without a note");
+                            let error_response =
+                                serde_json::to_string(&WebSocketResponse { response: 400 })
+                                    .unwrap();
+                            ws_tx
+                                .send(warp::ws::Message::text(error_response))
+                                .await
+                                .unwrap();
+                        }
+                    }
+                },
+                Err(e) => {
+                    info!("Failed to deserialize WebSocketMessage: {}", e);
+                    let response =
+                        serde_json::to_string(&WebSocketResponse { response: 400 }).unwrap();
+
+                    ws_tx.send(warp::ws::Message::text(response)).await.unwrap();
+                }
+            }
+        }
+    }
+}
